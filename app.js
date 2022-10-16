d0e5a7d4108981324f2e703a6f0d3fdb770442bd
zxw <769917560@qq.com>
2022-10-16 12:14:47 +0800

    添加收集bug

---
+++ app.js
@@ -0,0 +1,210 @@
+const got = require('got')
+const {
+  autoGame
+} = require('./autoGame')
+
+const {
+  cookie,
+  aid,
+  uuid,
+  _signature,
+  PUSH_PLUS_TOKEN,
+  DING_TALK_TOKEN,
+  uid
+} = require('./config')
+// console.log("app.js cookie", cookie)
+
+const BASEURL = 'https://api.juejin.cn/growth_api/v1/check_in' // 掘金签到api
+const PUSH_URL = 'http://www.pushplus.plus/send' // pushplus 推送api
+const DINGTALK_PUSH_URL = "https://oapi.dingtalk.com/robot/send?access_token=" + DING_TALK_TOKEN; // 钉钉webhook
+
+const URL = `${BASEURL}?aid=${aid}&uuid=${uuid}&_signature=${_signature}`
+const DRAW_URL = `https://api.juejin.cn/growth_api/v1/lottery/draw?aid=${aid}&uuid=${uuid}&_signature=${_signature}`
+const LUCKY_URL = `https://api.juejin.cn/growth_api/v1/lottery_lucky/dip_lucky?aid=${aid}&uuid=${uuid}`
+const DRAW_CHECK_URL = `https://api.juejin.cn/growth_api/v1/lottery_config/get?aid=${aid}&uuid=${uuid}`
+const NOT_COLLECT_URL = `https://api.juejin.cn/user_api/v1/bugfix/not_collect?aid=${aid}&uuid=${uuid}&spider=0`
+// https://api.juejin.cn/user_api/v1/bugfix/collect?aid=2608&uuid=6989117473007552032&spider=0
+const COLLECT_URL = `https://api.juejin.cn/user_api/v1/bugfix/collect?aid=${aid}&uuid=${uuid}&spider=0`
+
+
+const HEADERS = {
+  cookie,
+  'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67'
+}
+const HEADERS_DINGTALK_WEB_HOOK = {
+  "Content-Type": "application/json",
+};
+/**
+ * @desc 签到
+ */
+async function signIn() {
+  const res = await got.post(URL, {
+    hooks: {
+      beforeRequest: [
+        options => {
+          Object.assign(options.headers, HEADERS)
+        }
+      ]
+    }
+  })
+  // console.log('signIn', res.body)
+  const drawData = await got.get(DRAW_CHECK_URL, {
+    hooks: {
+      beforeRequest: [
+        options => {
+          Object.assign(options.headers, HEADERS)
+        }
+      ]
+    }
+  })
+  // console.log('drawData', JSON.parse(drawData.body).data)
+
+  if (JSON.parse(drawData.body).data.free_count > 0) draw(); // 免费次数大于0时再抽
+  lucky();
+  if (PUSH_PLUS_TOKEN || DING_TALK_TOKEN) {
+    if (typeof res.body == "string") res.body = JSON.parse(res.body);
+    const msg = res.body.err_no == 0 ? `成功，获得${res.body.data.incr_point}个矿石，矿石总数：${res.body.data.sum_point}个。` : "失败，" + res.body.err_msg;
+    handlePush(msg);
+  }
+  if (!uid) return;
+  autoGame();
+}
+/**
+ * @desc 抽奖
+ */
+async function draw() {
+  const res = await got.post(DRAW_URL, {
+    hooks: {
+      beforeRequest: [
+        options => {
+          Object.assign(options.headers, HEADERS)
+        }
+      ]
+    }
+  })
+  // console.log(res.body)
+}
+
+/**
+ * @desc 沾喜气
+ */
+async function lucky() {
+  const body = {
+    lottery_history_id: "7020267603864059917"
+  }
+  const res = await got.post(LUCKY_URL, {
+    hooks: {
+      beforeRequest: [
+        options => {
+          Object.assign(options.headers, HEADERS)
+        }
+      ]
+    },
+    json: body,
+  })
+  // console.log('lucky',res.body)
+}
+/**
+ * @desc 收集bug
+ */
+async function collectBug() {
+  const res = await got.post(NOT_COLLECT_URL, {
+    hooks: {
+      beforeRequest: [
+        options => {
+          Object.assign(options.headers, HEADERS)
+        }
+      ]
+    },
+  })
+  const bugList = JSON.parse(res.body).data;
+  bugList.map(item => {
+    const body = {
+      bug_time: item.bug_time,
+      bug_type: item.bug_type
+    }
+    // console.log('body', body)
+
+    const res = got.post(COLLECT_URL, {
+      hooks: {
+        beforeRequest: [
+          options => {
+            Object.assign(options.headers, HEADERS)
+          }
+        ]
+      },
+      json: body
+    })
+    // console.log('res', res)
+  })
+  // console.log('bugList', bugList)
+
+  // console.log('collectBug', res.body)
+}
+
+// push
+async function handlePush(desp) {
+  const url = DING_TALK_TOKEN == '' ? PUSH_URL : DINGTALK_PUSH_URL;
+  const body = DING_TALK_TOKEN == '' ? {
+    token: `${PUSH_PLUS_TOKEN}`,
+    title: `签到结果`,
+    content: `${desp}`
+  } : {
+    msgtype: "text",
+    text: {
+      content: "签到结果: " + desp
+    },
+  };
+
+  let param = {
+    json: body,
+  };
+  if (DING_TALK_TOKEN != '') {
+    param.hooks = {
+      beforeRequest: [
+        (options) => {
+          Object.assign(options.headers, HEADERS_DINGTALK_WEB_HOOK);
+        },
+      ],
+    }
+  }
+  const res = await got.post(url, param);
+  // console.log(res.body);
+}
+
+function randomNum(m, n) {
+  return minute = Math.floor(Math.random() * (m - n) + n);
+}
+/**
+ * 设置每日定时任务
+ * @param {*} config 配置参数的说明：
+ {
+    interval: 1, //间隔天数，间隔为整数
+    runNow: false, //是否立即运行
+    time: "14:00:00" //执行的时间点 时在0~23之间
+}
+ * @param {*} func 参数是要执行的方法。
+ * @param {*} params 请求头参数
+ * @param {*} headers 请求头参数
+ */
+function timeoutFunc(config, func) {
+  config.runNow && func()
+  let nowTime = new Date().getTime() //当前时间戳
+  let timePoints = config.time.split(':').map(i => parseInt(i))
+  let recent = new Date().setHours(...timePoints) //传入的执行时间时间戳
+  recent >= nowTime || (recent += 24 * 60 * 60 * 1000)
+  setTimeout(() => {
+    func()
+    setInterval(() => {
+      func()
+    }, config.interval * 24 * 60 * 60 * 1000)
+  }, recent - nowTime)
+}
+/* timeoutFunc({
+  interval: 1,
+  runNow: true,
+  // time: "08:" + randomNum(20, 30) + ":" + randomNum(20, 30)
+  time: "12:" + randomNum(10, 30) + ":" + randomNum(20, 30)
+  // time: "09:50:00"
+}, signIn) */
+collectBug()
---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // 微信
+  cookie: "_tea_utm_cache_2608=undefined; __tea_cookie_tokens_2608=%257B%2522user_unique_id%2522%253A%25226945012931944220163%2522%252C%2522web_id%2522%253A%25226945012931944220163%2522%252C%2522timestamp%2522%253A1649927241801%257D; _ga=GA1.2.881805595.1649983915; _gid=GA1.2.1228209897.1649983915; MONITOR_WEB_ID=188932cd-9d50-440c-97bb-13dce7b17eaa; passport_csrf_token=f9c1a48d3e250f3a9b791dcf6e40f1e4; passport_csrf_token_default=f9c1a48d3e250f3a9b791dcf6e40f1e4; n_mh=9-mIeuD4wZnlYrrOvfzG3MuT6aQmCUtmr8FxV8Kl8xY; sid_guard=9a9f8fc69b4d86e279e99433c3df4ef9%7C1649985006%7C31536000%7CSat%2C+15-Apr-2023+01%3A10%3A06+GMT; uid_tt=656546743749de98170aa7af4fd6a8e4; uid_tt_ss=656546743749de98170aa7af4fd6a8e4; sid_tt=9a9f8fc69b4d86e279e99433c3df4ef9; sessionid=9a9f8fc69b4d86e279e99433c3df4ef9; sessionid_ss=9a9f8fc69b4d86e279e99433c3df4ef9; sid_ucp_v1=1.0.0-KDk1OTEwOTkzN2FjMzM4YTMzMjE3ZGYyZGY1YmViMjExYWI3NWUwZGMKFgjH68C-_fX5ARDui-OSBhiwFDgIQAsaAmxmIiA5YTlmOGZjNjliNGQ4NmUyNzllOTk0MzNjM2RmNGVmOQ; ssid_ucp_v1=1.0.0-KDk1OTEwOTkzN2FjMzM4YTMzMjE3ZGYyZGY1YmViMjExYWI3NWUwZGMKFgjH68C-_fX5ARDui-OSBhiwFDgIQAsaAmxmIiA5YTlmOGZjNjliNGQ4NmUyNzllOTk0MzNjM2RmNGVmOQ",
+  PUSH_PLUS_TOKEN: '',
+  aid: '2608',
+  uuid: '6945012931944220163',
+  _signature: '',
+  DING_TALK_TOKEN: '', // 钉钉webhook参考（很简单）：https://open.dingtalk.com/document/group/custom-robot-access
+  uid: '1099167356171719', // *自动玩游戏需要此参数，在掘金首页打开控制台输入这行代码`window.__NUXT__.state.auth.user.id`就可以得到
+  User_Agent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:90.0) Gecko/20100101 Firefox/90.0"
+  // GitHub
+  /* cookie: "ttcid=09bdba82298346048dd2f83cd8aca8b518; MONITOR_WEB_ID=7a6a5403-09a6-4cf2-aa07-7f872fae54b4; _ga=GA1.2.1524270758.1627280724; sid_guard=1c349f34964ac6c24d0d72bfb4ef455c%7C1637998300%7C5184000%7CWed%2C+26-Jan-2022+07%3A31%3A40+GMT; n_mh=0Lqru5PxxsC-v0CJZNRqWrDwVujbTzKV4ajwYN8kSVc; passport_csrf_token_default=bf2e9347ae951a509aaefe98fe3a6c4b; passport_csrf_token=bf2e9347ae951a509aaefe98fe3a6c4b; uid_tt=2dbc29a062e349b25ca4ea5e1de92235; uid_tt_ss=2dbc29a062e349b25ca4ea5e1de92235; sid_tt=1c349f34964ac6c24d0d7…xNDRmNjg5OWQ2YzA2ODJkNGYKFgj9_NC__fW9BRDcvYeNBhiwFDgIQDgaAmxmIiAxYzM0OWYzNDk2NGFjNmMyNGQwZDcyYmZiNGVmNDU1Yw; __tea_cookie_tokens_2608=%257B%2522user_unique_id%2522%253A%25226989117473007552032%2522%252C%2522web_id%2522%253A%25226989117473007552032%2522%252C%2522timestamp%2522%253A1642240249124%257D; _gid=GA1.2.85615495.1642987366; _tea_utm_cache_2608={%22utm_medium%22:%22user_center%22%2C%22utm_campaign%22:%22hdjjgame%22}; tt_scid=yKE7OMJkimlRax4BSFM5MZNzofqFuCUMj2Yx5r7nilUXTccAmCvdB44d-145XbIPcd72; _gat=1",
+  PUSH_PLUS_TOKEN: '',
+  aid: '2608',
+  uuid: '6989117473007552032',
+  _signature: '',
+  DING_TALK_TOKEN: '', // 钉钉webhook参考（很简单）：https://open.dingtalk.com/document/group/custom-robot-access
+  uid: '3087084381552253', // *自动玩游戏需要此参数，在掘金首页打开控制台输入这行代码`window.__NUXT__.state.auth.user.id`就可以得到
+  User_Agent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:90.0) Gecko/20100101 Firefox/90.0" */
+}
