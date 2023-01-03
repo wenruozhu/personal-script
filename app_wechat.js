a28cfc81e05b6bef9b34e702ffbc57954f7dd8ce
zxw <769917560@qq.com>
2023-01-03 11:21:07 +0800

    执行完重置数据

--- app_wechat.js
+++ app_wechat.js
@@ -34,7 +34,7 @@ const HEADERS_DINGTALK_WEB_HOOK = {
   "Content-Type": "application/json",
 };
 
-const growth = {
+let growth = {
   checkedIn: false, // 是否签到
   incrPoint: 0, // 签到获得矿石数
   sumPoint: 0, // 总矿石数
@@ -308,8 +308,23 @@ function runAllFn() {
   setTimeout(async () => {
     if (!uid) return;
     await autoGame('wechat');
-  }, getRandomArbitrary(500000, 600000));
-
+  }, getRandomArbitrary(150000, 160000));
+  // 执行完重置所有值
+  growth = {
+    checkedIn: false, // 是否签到
+    incrPoint: 0, // 签到获得矿石数
+    sumPoint: 0, // 总矿石数
+    // contCount: 0, // 连续签到天数
+    // sumCount: 0, // 累计签到天数
+    dippedLucky: false, // 是否沾喜气
+    dipValue: 0, // 幸运值
+    luckyValue: 0, // 总幸运值
+    // freeCount: 0, // 免费抽奖次数
+    freeDrawed: false, // 是否免费抽奖
+    lotteryName: '', // 奖品名称
+    collectedBug: false, // 是否收集 Bug
+    collectBugCount: 0, // 收集 Bug 的数量
+  }
 }
 
 /**
